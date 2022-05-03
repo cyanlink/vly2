76c2fe9c78a07edd7d580a0e49987ebee80f9372
cyanlink <tgvita@outlook.com>
2022-05-03 22:14:30 +0800

    router.query.type is undefined, change to actType to make it work as intended

--- components/Act/ActMenu.js
+++ components/Act/ActMenu.js
@@ -65,7 +65,7 @@ const ActMenu = ({ acts, onClick }) => {
           })}
 
           <Menu.Item key='back'>
-            <Link href={`/acts/type/${router.query.type}`}>
+            <Link href={`/acts/type/${router.query.actType}`}>
               See all
             </Link>
           </Menu.Item>
